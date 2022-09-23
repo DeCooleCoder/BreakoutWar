ed8d1ba2170734d2831810b8335efb549a14aebc
Error734862947 <93121855+B2710@users.noreply.github.com>
2022-09-23 08:28:21 +0000

    Ball class done

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
 class Ball{
-    constructor(){
-        
+    constructor(x,y,w,h,vx,vy,c){
+        this.x = x;
+        this.y = y;
+        this.width = w;
+        this.height = h;
+        this.vx = vx;
+        this.vy = vy;
+        this.color = c;
+    }
+
+    draw(){
+        fill(this.color);
+        ellipse(this.x, this.y, this.width, this.height);
+        this.x = this.x + this.vx;
+        this.y = this.y + this.vy;
+
+        if(this.x < 0 || this.x > width){
+            this.vx = this.vx * -1
+        }
+        if(this.y < 0 || this.y > height){
+            this.vy = this.vy * -1
+        }
     }
 }
 
-class Brick{
+var Ball1
+
+class Block{
     constructor(){
         
     }
@@ -12,8 +34,10 @@ class Brick{
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  Ball1 = new Ball(70,190,30,30,5,5,"crimson")
 }
 
 function draw() {
-    
+    background('black')
+    Ball1.draw()
 }

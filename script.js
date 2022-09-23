8b750b049665ebaaffb4d074f26e97d76925d7e5
Error734862947 <93121855+B2710@users.noreply.github.com>
2022-09-23 08:08:45 +0000

    removed all code except functions and Canvas

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
-let colorlist = ['white', 'black']
-
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background('gray');
 }
 
 function draw() {
-  noStroke()
-  fill(random(colorlist));
-  ellipse(mouseX, mouseY, 50, 50);
+    
 }

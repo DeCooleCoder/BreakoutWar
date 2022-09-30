5b94620c093869c335fd9b4582352619492559f7
Joshua Langelaar <93121879+DeCooleCoder@users.noreply.github.com>
2022-09-30 11:06:26 +0000

    class voor de bricks gecreerd

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-class player{
-  constructor( x, y, w, h, c){
+//Classes!!!
+class player {
+  constructor(x, y, w, h, c) {
     this.x = x;
     this.y = y;
     this.width = w;
@@ -9,12 +10,12 @@ class player{
   draw() {
     fill(this.color);
     rect(this.x, this.y, this.width, this.height);
-   }
   }
+}
 
-class ball{
-  
-  constructor( x, y, w, h, vx, vy, c){
+class ball {
+
+  constructor(x, y, w, h, vx, vy, c) {
     this.x = x;
     this.y = y;
     this.width = w;
@@ -24,65 +25,75 @@ class ball{
     this.color = c;
   }
 
-    draw() {
-      fill(this.color);
+  draw() {
+    fill(this.color);
     ellipse(this.x, this.y, this.width, this.height);
     this.x = this.x + this.vx;
     this.y = this.y + this.vy;
 
-    if(this.x < -10 || this.x > width){
+    if (this.x < -10 || this.x > width) {
       this.vx = this.vx * -1;
     }
-    if(this.y < -10 || this.y > height){
+    if (this.y < -10 || this.y > height) {
       this.vy = this.vy * -1;
     }
   }
 }
 
-class Platform{
-    constructor(x,y,c){
-        Platform.x = x;
-        Platform.y = y;
-        Platform.color = c;
-    }
+class Platform {
+  constructor(x, y, c) {
+    Platform.x = x;
+    Platform.y = y;
+    Platform.color = c;
+  }
 
-    draw(){
-        fill(Platform.color);
-        rect(Platform.x, Platform.y, 80, 20);
-    }
+  draw() {
+    fill(Platform.color);
+    rect(Platform.x, Platform.y, 80, 20);
+  }
 
-    hit(){
-              
-    }
+  hit() {
+
+  }
+}
+
+class brick{
+  constructor( x, y, w, h){
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+  }
 }
 
 //Variables!!!
 var Ball1;
 var Platform1;
 
+//Functies!!!
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  Ball1 = new Ball(windowWidth/2,windowHeight/2,30,30,5,5,"crimson");
-  Platform1 = new Platform(windowWidth/2,windowHeight/2, "white");
+  Ball1 = new Ball(windowWidth / 2, windowHeight / 2, 30, 30, 5, 5, "crimson");
+  Platform1 = new Platform(windowWidth / 2, windowHeight / 2, "white");
   // for(getal = 0; getal < 30; getal += 1){
   //   blocks.push(new Block());
   // }
 }
 
 function draw() {
-    background('black');
-    Platform1.draw();
-    Ball1.draw();
-    
-    // blocks.forEach(block => {
-    //  block.draw()
-    // }); 
+  background('black');
+  Platform1.draw();
+  Ball1.draw();
 
-    if (keyIsDown(37)){
-            Platform.x =  Platform.x - 5  
-        }
+  // blocks.forEach(block => {
+  //  block.draw()
+  // }); 
 
-        if (keyIsDown(39)){
-            Platform.x = Platform.x + 5
-        }  
+  if (keyIsDown(37)) {
+    Platform.x = Platform.x - 5
+  }
+
+  if (keyIsDown(39)) {
+    Platform.x = Platform.x + 5
+  }
 }

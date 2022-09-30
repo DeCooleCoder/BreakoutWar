81fa8fc866e712804015421ddc126e893567f554
Error734862947 <93121855+B2710@users.noreply.github.com>
2022-09-30 08:08:40 +0000

    created platform and basic movement of platform

--- script.js
+++ script.js
@@ -1,30 +1,54 @@
 class Ball{
     constructor(x,y,w,h,vx,vy,c){
-        this.x = x;
-        this.y = y;
-        this.width = w;
-        this.height = h;
-        this.vx = vx;
-        this.vy = vy;
-        this.color = c;
+        Ball.x = x;
+        Ball.y = y;
+        Ball.width = w;
+        Ball.height = h;
+        Ball.vx = vx;
+        Ball.vy = vy;
+        Ball.color = c;
     }
 
     draw(){
-        fill(this.color);
-        ellipse(this.x, this.y, this.width, this.height);
-        this.x = this.x + this.vx;
-        this.y = this.y + this.vy;
+        fill(Ball.color);
+        ellipse(Ball.x, Ball.y, Ball.width, Ball.height);
+        Ball.x = Ball.x + Ball.vx;
+        Ball.y = Ball.y + Ball.vy;
 
-        if(this.x < 0 || this.x > width){
-            this.vx = this.vx * -1
+        if(Ball.x < 0 || Ball.x > width){
+            Ball.vx = Ball.vx * -1
         }
-        if(this.y < 0 || this.y > height){
-            this.vy = this.vy * -1
+        if(Ball.y < 0 || Ball.y > height){
+            Ball.vy = Ball.vy * -1
         }
     }
 }
 
 var Ball1;
+
+class Platform{
+    constructor(a,b,c){
+        Platform.a = a;
+        Platform.b = b;
+        Platform.color = c;
+    }
+
+    draw(){
+        fill(Platform.color);
+        rect(Platform.a, Platform.b, 80, 20);
+
+        if ((keyIsPressed == true) && (key === Right_Arrow)) {
+            Platform.a = Platform.a - 5
+            
+        }
+        if ((keyIsPressed == true) && (key === Left_Arrow)) {
+            Platform.a = Platform.a + 5
+            
+        }
+    }
+}
+
+var Platform1;
 
 // class Block{
 //     constructor(){
@@ -42,8 +66,8 @@ var Ball1;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  Ball1 = new Ball(70,190,30,30,5,5,"crimson");
-
+  Ball1 = new Ball(windowWidth/2,windowHeight/2,30,30,5,5,"crimson");
+  Platform1 = new Platform(windowWidth/2,windowHeight/2,"white");
   // for(i = 0; i < 30; i++){
   //   blocks.push(new Block());
   // }
@@ -51,7 +75,10 @@ function setup() {
 
 function draw() {
     background('black');
+    Platform1.draw();
     Ball1.draw();
+
+    
     
     // blocks.forEach(block => {
     //  block.draw()
